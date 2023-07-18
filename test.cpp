7a7303daa3493ec59fdb01acbf074652eef53384
teaaagyu <139877404+teaaagyu@users.noreply.github.com>
2023-07-18 22:23:25 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,5 +7,7 @@ int main(){
 	return 0;
 }
 
+//안녕
+
 // https://velog.io/@youhyeoneee/%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95-VS-Code-%EC%97%90%EC%84%9C-CC-%EC%BD%94%EB%94%A9-%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%B6%95%ED%95%98%EA%B8%B0-Windows
 // https://velog.io/@astar5327/gcc-%EC%BB%B4%ED%8C%8C%EC%9D%BC-%EC%97%90%EB%9F%AC-No-such-file-or-directory-gcc.exe-%ED%95%B4%EA%B2%B0%EB%B2%95
